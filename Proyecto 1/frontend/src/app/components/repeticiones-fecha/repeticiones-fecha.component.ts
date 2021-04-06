4585e0cf6a4cd19201f69c49b4475ed4d6074208
Romario David Castillo Echeverría <romariocastillo1994@gmail.com>
2021-04-06 12:43:07 -0600

    se agrego reporte por fechas

--- Proyecto 1/frontend/src/app/components/repeticiones-fecha/repeticiones-fecha.component.ts
+++ Proyecto 1/frontend/src/app/components/repeticiones-fecha/repeticiones-fecha.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { LecturaService } from 'src/app/services/lecturaServices/lectura.service'
+import { Router } from '@angular/router';
+import { Usuario } from 'src/app/models/Usuario';
+
 
 @Component({
   selector: 'app-repeticiones-fecha',
@@ -7,9 +11,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RepeticionesFechaComponent implements OnInit {
 
-  constructor() { }
+  objRes:any
+  dato: string = ""
+  selectednombre:any
+  obResP:any
+  obResMin:any
+  obResMax:any
+
+  constructor(private lecturaService:LecturaService, private router:Router) { }
 
   ngOnInit(): void {
+    this.selectednombre = localStorage.getItem('nombreAtletaGrafica')
+    
+    this.dato = 'Velocidad alcanzada';
+
+    let elemento = document.querySelector<HTMLElement>('h1');
+    if (elemento != undefined  && elemento != null){
+      console.log(elemento.innerHTML)
+      elemento.innerHTML = this.dato + " de " + this.selectednombre
+    }
+    this.showConteo()
+    this.showRepeticiones()
+  }
+  showConteo(){
+    let usuarioActivo = localStorage.getItem('usuarioActivo');
+    if((usuarioActivo == null  ||  usuarioActivo==undefined)){
+      this.router.navigate(['']);
+      return;
+    }
+    let atleta:Usuario = <Usuario>JSON.parse(usuarioActivo);
+    if(atleta.iduser == null){
+      return;
+    }
+    let idSelected = localStorage.getItem('idAtletaGrafica');
+    if(idSelected != null){
+      this.lecturaService.getReportConteo(Number(idSelected)).subscribe((res) => {
+        this.objRes = res
+      },
+      err=>{
+        alert(err.respuesta);
+      })
+    }
+  }
+
+  showRepeticiones(){
+    let idSelected = localStorage.getItem('idAtletaGrafica');
+    if(idSelected != null){
+      this.lecturaService.getReportRepeticionesProm(Number(idSelected)).subscribe((res) => {
+        //console.log(res)
+        let elemento = <HTMLElement>document.getElementById('promedio')
+        if(elemento != undefined && elemento != null && res.dato != null){
+          elemento.innerHTML = String(res.dato)
+        }
+      },
+      err=>{
+        alert(err.respuesta);
+      })
+      this.lecturaService.getReportRepeticionesMin(Number(idSelected)).subscribe((res) => {
+        let elemento = <HTMLElement>document.getElementById('minimo')
+        if(elemento != undefined && elemento != null && res.dato != null){
+          elemento.innerHTML = String(res.dato)
+        }
+      },
+      err=>{
+        alert(err.respuesta);
+      })
+      this.lecturaService.getReportRepeticionesMax(Number(idSelected)).subscribe((res) => {
+        let elemento = <HTMLElement>document.getElementById('maximo')
+        if(elemento != undefined && elemento != null && res.dato != null){
+          elemento.innerHTML = String(res.dato)
+        }
+      })
+    }
   }
 
 }
