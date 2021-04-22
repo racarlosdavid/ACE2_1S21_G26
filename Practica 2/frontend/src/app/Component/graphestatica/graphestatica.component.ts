88140d35910cc41ee5d0a21ce7ad06625472e255
Romario Castillo <2447479030101@ingenieria.usac.edu.gt>
2021-04-22 15:29:31 -0600

    Se agrego la gráfica estática.

--- Practica 2/frontend/src/app/Component/graphestatica/graphestatica.component.ts
+++ Practica 2/frontend/src/app/Component/graphestatica/graphestatica.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Params,Router } from '@angular/router';
 import { Chart } from 'chart.js';
+import { RegistroNow } from 'src/app/models/registro-now';
 import { LecturaService } from 'src/app/services/lecturaServices/lectura.service'
 
 @Component({
@@ -10,30 +11,35 @@ import { LecturaService } from 'src/app/services/lecturaServices/lectura.service
 })
 export class GraphestaticaComponent implements OnInit {
 
-  //public nombreAtletaGrafica;
-  /**
-  * Interval to update the chart
-  * @var {any} intervalUpdate
-  */
-   private intervalUpdate: any = null;
-
    /**
    * The ChartJS Object
    * @var {any} chart
    */
-   public chartEstatica: any = null;
+   public chartEstatica: Chart;
 
-  constructor(private router:Router,private lectura:LecturaService) { }
+  constructor(private router:Router,private lectura:LecturaService,private activatedR: ActivatedRoute) {}
 
   ngOnInit(): void {
-    
+    this.crearGrafica()
+    this.activatedR.params.subscribe((params:Params) =>{
+      //console.log(params)
+      if(params.idtest != undefined){
+        //console.log('Definido')
+        this.showData(params.idtest)
+      }else{
+        console.log('sin parametro')
+      }
+    });
+  }
+
+  crearGrafica(){
     this.chartEstatica = new Chart('chartVolumen', {
       type: 'line',
       data: {
        labels: [],
        datasets: [
          {
-        label: 'Temperatura Corporal',
+        label: 'VO2 MAX',
         fill: false,
         data: [],
         backgroundColor: '#ECEF16',
@@ -67,7 +73,36 @@ export class GraphestaticaComponent implements OnInit {
        }
         }
      });
-
   }
 
+  private showData(idtest:String): void{
+    this.removeData()
+    this.lectura.getLecturaGrafica(1).subscribe((res) =>{
+      let Res = <RegistroNow[]>res
+      //console.log(Res)
+      for(let item of Res){
+        if(item.idtest == Number(idtest)){
+          //console.log(item.fecha + ' ' + item.hora + ' ' + item.inhala + ' ' + item.exhala)
+          if(item.inhala == 0 && item.exhala == 0){
+            console.log('ambos son cero')
+          }else{
+            this.chartEstatica.data.labels.push(item.hora)
+            if(item.inhala != 0){
+              this.chartEstatica.data.datasets[0].data.push(item.inhala);
+              this.chartEstatica.update();
+              }
+            if(item.exhala != 0){
+              this.chartEstatica.data.datasets[0].data.push(item.exhala * -1);
+              this.chartEstatica.update();
+            }
+          }
+        }
+      }
+    })
+  }
+  removeData(){
+    this.chartEstatica.data.labels = []
+    this.chartEstatica.data.datasets[0].data = []
+    this.chartEstatica.update()
+  }
 }
